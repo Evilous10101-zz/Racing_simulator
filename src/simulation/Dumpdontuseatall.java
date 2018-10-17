6d688ea16e1bbd25dadd92228ce6a710c197af3d
Michael Lannon <sMichaelLannon@yccc.edu>
2018-10-17 19:35:41 -0400

    update

--- src/simulation/Dumpdontuseatall.java
+++ src/simulation/Dumpdontuseatall.java
@@ -1,7 +1,18 @@
 package simulation;
 
 public class Dumpdontuseatall {
+	//This is code that I am saving just in case it is needed
 	static Track distance = new Track();
 	static Track turns = new Track();
 	static Track type = new Track();
+	
+	
+	private static final int desert = 0;
+	private static final int forest = 0;
+	private static final int urban = 0;
+	private static final int ocean = 0;
+	
+	private static final int buick = 0;
+	private static final int hybrid = 0;
+	private static final int sedan = 0;
 }
--- src/simulation/Mainmethod.java
+++ src/simulation/Mainmethod.java
@@ -1,30 +1,24 @@
 package simulation;
 import java.util.Scanner;
 public class Mainmethod {
-	private static final int desert = 0;
-	private static final int forest = 0;
-	private static final int urban = 0;
-	private static final int ocean = 0;
+	
 	//creates track type variables
 	
-	private static final int buick = 0;
-	private static final int hybrid = 0;
-	private static final int sedan = 0;
+	
 	//creates types of cars
 	
 	//creates random data for each car based on the numbers below
-	static int[] car_type = {buick, hybrid, sedan};
-	static int[] topspeed = {70, 100, 200};
+	static String[] car_type = {"buick", "hybrid", "sedan"};
+	static int[] topspeed = {70, 100, 200, 250};
 	static int[] fuel_tank_amount = {100, 200, 150, 50};
 	static int[] mpg_amount = {10, 20, 30, 50};
 	static int[] miles_until_empty = {100, 50, 75, 125};
-	
-	
-	
+	//Creates data on the track
 	public static int[] track_distance = {100, 90, 80, 70, 60, 50};
+	public static String[] track_type = {"desert", "forest", "urban", "ocean"};
+	//Creates data on the race itself
 	public static int[] track_turns = {5, 4, 3, 2, 1};
-	public static int[] track_type = {desert, forest, urban, ocean};
-	static int[] time_limit = {300};
+	static int[] time_limit = {250};
 	static int[] winner = {1};
 	//gets track with it's distance and turn amount with time limit
 	
@@ -32,27 +26,45 @@ public class Mainmethod {
 	
 	public static void main(String[] args) {
 		//Prints out items about the track, car, and and the race
-		System.out.println("Hello, welcome to the race! Here is some data about the race with your car! ");
+		System.out.println("Hello, welcome to the race! Here is some data about the race with your car!");
+		System.out.println("");
+		System.out.println("This is the data on the track:");
+		System.out.println("This is the type of track you are using: " + track_type);
 		
-		System.out.println("this is the type of track you are using: " + track_type);
+		System.out.println(java.util.Arrays.toString(topspeed));
 		
-		System.out.println("this is how many turns the track has: " + track_turns);
+		System.out.println("This is how many turns the track has: " + track_turns);
 		
-		System.out.println("this is the distance of every lap in the track: " + track_distance);
-		
-		System.out.println("this is the car you are using: " + car_type);
+		System.out.println("This is the distance of every lap in the track: " + track_distance);
+		System.out.println("-----------------");
+		System.out.println("This is the data on the car");
+		System.out.println("This is the car you are using: " + car_type);
 		
 		System.out.println("This is the mpg of your car: "+ mpg_amount);
 		
 		System.out.println("This is the fuel tank ammount you start with: " + fuel_tank_amount);
 		
-		System.out.println("this is the top speed your car get get to :" + topspeed);
+		System.out.println("This is the top speed your car get get to :" + topspeed);
 		
 		System.out.println("This is the ammount of miles you can go before your tank is empty: "+ miles_until_empty);
+		System.out.println("-----------------------");
+		System.out.println("This is the data on the race itself:");
+		System.out.println("You have this amount of seconds before the race ends: "+ time_limit);
 		
-		System.out.println("you have this ammount of seconds before the race ends: "+ time_limit);
-		
-		System.out.println("there can be this many winners: " + winner);
+		System.out.println("There can be this many winners: " + winner);
+		System.out.println("------------------------------");
+		//States how you are going to win, if you can
+		int t = (track_distance * track_turns);
+		System.out.println("This is the ammount of miles overall you have to go though: " + t);
+		System.out.println("This is the ammount of hours it is going to take to finish the race: " + t / topspeed);
+		System.out.println("This is the rate of loss of this car: " + mpg_amount / topspeed);
+		System.out.println("This is the amount of time it will take to finish the race");
+		if(mpg_amount + topspeed > fuel_tank_amount)
+		{
+			System.out.println("You have to stop and get gas");
+			//adds time to time limit
+			
+		}
 	
 	}
 }
